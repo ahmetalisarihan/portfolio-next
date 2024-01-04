0ec8bb9956d3cb376717e236e4722b6a3b52a65f
ahmetalisarihan <ahmetalisarihan@gmail.com>
2024-01-04 23:48:11 +0300

    Add hero section

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { ArrowRight, Phone } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -7,7 +8,7 @@ const page = () => {
     // hero section
     <section className="flex items-center max-width flex-col-reverse tablet:flex-row mb-10">
       {/* Left side */}
-      <div className="tablet:w:2/3 mt-10 tablet:mt-0">
+      <div className="tablet:w-2/3 mt-10 tablet:mt-0">
         <div className="flex flex-col gap-3 items-center tablet:items-start max-w-2xl text-center tablet:text-left">
           <p className="px-4 py-2 bg-peach-500 text-gray-600 w-max rounded">
             Hello There
@@ -32,7 +33,11 @@ const page = () => {
         </div>
       </div>
       {/* Right side */}
+      <div className="relative w-full tablet:w-1/3 h-[400px] tablet:h-[700px] rounded-b-2xl overflow-hidden">
+        <Image src={"/images/hero.png"} alt="hero" fill className="object-cover" />
+      </div>
     </section>
+    // skills section
   );
 };
 
